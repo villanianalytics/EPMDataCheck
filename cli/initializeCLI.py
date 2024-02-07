4267bca03de7101de6a2faa4a1c709402dbbe9d6
Villani Analytics <dvillani@villytics.com>
2024-02-07 16:04:16 -0500

    enhanced xlsx comparison

--- cli/initializeCLI.py
+++ cli/initializeCLI.py
@@ -31,7 +31,7 @@ def run_cli():
     parser_save_to_excel.add_argument('--excel-destination', help='Path to save Excel file', type=str)
     parser_save_to_excel.add_argument('--srcHeaders', help='Number of Source Header Rows', type=str)
     parser_save_to_excel.add_argument('--tgtHeaders', help='Number of Target Header Rows', type=str)
-    parser_save_to_excel.add_argument('--rowDims', help='Row Dimension members', type=str)
+    parser_save_to_excel.add_argument('--rowDims', help='Comma separated list of Row Dimension names', type=str)
 
     parser_export_slice = subparsers.add_parser('export-data-slice', help='Exports data from an Oracle EPM application')
     parser_export_slice.add_argument('--base_url', help='base application URL', type=str)
@@ -104,13 +104,16 @@ def run_cli():
                     args.row_dimensions)
 
     elif args.command == 'save-to-excel':
-
+        if args.rowDims:
+            row_dimensions = args.rowDims.split(',')
+        else:
+            row_dimensions = []
         save_to_excel_with_hash_check(args.source,
                                       args.target,
                                       args.excel_destination,
                                       args.srcHeaders,
                                       args.tgtHeaders,
-                                      args.rowDims)
+                                      row_dimensions)
 
     elif args.command == 'export-data-slice':
         export_data_slice_json(
